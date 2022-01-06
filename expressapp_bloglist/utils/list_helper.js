6af8146dd361a6cd308088e468fc7d65bfcc1f22
henna <henna.lehtinen91@gmail.com>
2022-01-06 19:59:32 +0200

    Did task 4.4: made a function totalLikes and defined tests for it.

--- expressapp_bloglist/utils/list_helper.js
+++ expressapp_bloglist/utils/list_helper.js
@@ -1,5 +1,14 @@
+
 const dummy = blogs => 1
 
+const totalLikes = blogs => {
+  totalAmount = blogs.reduce((sum, blog) => sum + blog.likes, 0)
+  return totalAmount
+}
+
+
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
