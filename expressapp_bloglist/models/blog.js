46b65187154c52c579158b176f924dcb3165ea8c
henna <henna.lehtinen91@gmail.com>
2022-01-06 16:40:38 +0200

    Did task 4.2: divided the code into modules.

---
+++ expressapp_bloglist/models/blog.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+
+const blogSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  url: {
+    type: String,
+    required: true,
+  },
+  author: String,
+  likes: Number
+})
+
+
+module.exports = mongoose.model('Blog', blogSchema)
