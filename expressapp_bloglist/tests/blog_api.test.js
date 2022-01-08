34b3c17fadc9b7f1b64d4e16859dd33a9eede121
henna <henna.lehtinen91@gmail.com>
2022-01-08 18:43:36 +0200

    Did task 4.12: made a test for HTTP post method to check that it is answered with status code 400 in case url or title are missing.

--- expressapp_bloglist/tests/blog_api.test.js
+++ expressapp_bloglist/tests/blog_api.test.js
@@ -12,72 +12,106 @@ beforeEach(async () => {
 })
 
 
-test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-})
 
+describe('when viewing all notes from database', () => {
 
-test('all blogs are returned', async () => {
-  const response = await api.get('/api/blogs')
-
-  expect(response.body).toHaveLength(helper.listWithManyBlogs.length)
-})
-
-
-test('the blogs have a field called id', async () => {
-  const response = await api.get('/api/blogs')
-
-  _.forEach(response.body, (value) => {
-    expect(value.id).toBeDefined()
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
   })
 
-  // option to using _.forEach in case the database is very large: 
-  // check only the first blog in the list of blogs
-  /*
-  const blogToInspect = response.body[0] // first blog in the list
-  expect(blogToInspect.id).toBeDefined()
-  */
+
+  test('all blogs are returned', async () => {
+    const response = await api.get('/api/blogs')
+
+    expect(response.body).toHaveLength(helper.listWithManyBlogs.length)
+  })
+
+
+  test('the blogs have a field called id', async () => {
+    const response = await api.get('/api/blogs')
+
+    _.forEach(response.body, (value) => {
+      expect(value.id).toBeDefined()
+    })
+
+    // option to using _.forEach in case the database is very large: 
+    // check only the first blog in the list of blogs
+    /*
+    const blogToInspect = response.body[0] // first blog in the list
+    expect(blogToInspect.id).toBeDefined()
+    */
+  })
 })
 
 
-test('a valid blog can be posted to the database', async () => {
-  const newBlog = helper.listWithOneBlog[0]
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
+describe('addition of a new blog', () => {
+
+  test('succeeds with valid data', async () => {
+    const newBlog = helper.listWithOneBlog[0]
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+
+      const blogsAtEnd = await helper.blogsInDb()
+      expect(blogsAtEnd).toHaveLength(helper.listWithManyBlogs.length + 1)
+
+      const titles = blogsAtEnd.map(b => b.title)
+      expect(titles).toContain(
+        'Go To Statement Considered Harmful'
+      )
+  })
+
+
+  test('with "likes" field left blank should have zero likes', async () => {
+    const newBlog = {
+      title: 'The Closet Historian',
+      url: 'http://theclosethistorian.blogspot.com/',
+      author: 'Bianca'
+    }
+
+    await api 
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(200)
 
     const blogsAtEnd = await helper.blogsInDb()
-    expect(blogsAtEnd).toHaveLength(helper.listWithManyBlogs.length + 1)
 
-    const titles = blogsAtEnd.map(b => b.title)
-    expect(titles).toContain(
-      'Go To Statement Considered Harmful'
-    )
+    const blogIndex = _.findIndex(blogsAtEnd, (blog) => blog.title === 'The Closet Historian')
+    expect(blogsAtEnd[blogIndex].likes).toBe(0)
+  })
+
+
+  test('fails with status code 400 if the fields "title" or "url" are left blank', async () => {
+    const newBlog = {
+      title: 'Sopivasti Hukassa',
+      author: 'Suvi',
+      likes: 12
+    }
+    await api 
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(400)
+
+
+    const newBlog2 = {
+      url: 'https://peculiarseamstress.blog/',
+      author: 'Sarah W.',
+      likes: 14
+    }
+    await api 
+      .post('/api/blogs')
+      .send(newBlog2)
+      .expect(400)
+  })
 })
 
-test('if the number of likes is left blank it should be zero', async () => {
-  const newBlog = {
-    title: 'The Closet Historian',
-    url: 'http://theclosethistorian.blogspot.com/',
-    author: 'Bianca'
-  }
-
-  await api 
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(200)
-
-  const blogsAtEnd = await helper.blogsInDb()
-  
-  const blogIndex = _.findIndex(blogsAtEnd, (blog) => blog.title === 'The Closet Historian')
-  expect(blogsAtEnd[blogIndex].likes).toBe(0)
-})
 
 
 afterAll(() => {
