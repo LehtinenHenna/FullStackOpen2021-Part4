39e7623b6a3f04c7ec68d4c3506cb3b8e81f2075
henna <henna.lehtinen91@gmail.com>
2022-01-09 19:37:57 +0200

    Did task 4.19: changed blog POST method so that it requires a valid token to succeed, and the owner of the token gets added to the user field of the posted blog.

--- expressapp_bloglist/controllers/blogs.js
+++ expressapp_bloglist/controllers/blogs.js
@@ -2,6 +2,7 @@ const blogsRouter = require('express').Router()
 const { request } = require('../app')
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 // gets all blogs
 blogsRouter.get('/', async (request, response) => {
@@ -11,12 +12,25 @@ blogsRouter.get('/', async (request, response) => {
   response.json(blogs.map(blog => blog.toJSON()))
 })
 
+// gets a token
+const getTokenFrom = request => {  
+  const authorization = request.get('authorization')  
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {    
+    return authorization.substring(7)  
+  }  
+  return null
+}
 // posts a new blog
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
-  const users = await User.find({})
-  const user = users[0]
-  console.log('user: ', user)
+
+  const token = getTokenFrom(request)
+  const decodedToken = jwt.verify(token, process.env.SECRET)  
+  
+  if (!token || !decodedToken.id) {    
+    return response.status(401).json({ error: 'token missing or invalid' })  
+  }  
+  const user = await User.findById(decodedToken.id)
 
   const blog = new Blog({
     title: body.title,
